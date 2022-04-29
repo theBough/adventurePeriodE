12d2935d5a15334e7f114561b5f360ce7403097e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-29 15:04:22 -0500

    check for screen change

--- sketch.js
+++ sketch.js
@@ -8,4 +8,15 @@ function draw() {
   background(220);
   p.display()
   p.update()
+  screenChange();
 }
+
+function screenChange(){
+  /*this function is checking to see if the player has gone off
+  the canvas*/
+  if(p.x < 0){
+    //off the canvas tothe left
+    //so place the player on the right
+    p.x = width - p.w
+  }
+}//end screen change
