820bc101a4bb48669b37de9619695ae03f165548
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-29 14:35:33 -0500

    Create player.js

---
+++ player.js
@@ -0,0 +1,10 @@
+function Player(x, y, w, h){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  
+  this.display = function(){
+    rect(this.x, this.y, this.w, this.h);
+  }//end display
+}//end Player
