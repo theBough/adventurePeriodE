28c8929bdc250fdf231ff5eb50cc8ac8930f952b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-29 14:47:35 -0500

    Update player.js

--- player.js
+++ player.js
@@ -7,4 +7,19 @@ function Player(x, y, w, h){
   this.display = function(){
     rect(this.x, this.y, this.w, this.h);
   }//end display
+  
+  this.update = function(){
+    /*This function will check to see if one of 
+    the arrows keys has been pressed
+    */
+    if(keyIsDown(37)){
+      //this is the left arrow key
+      this.x -= 5;
+    }//end if
+    if(keyIsDown(39)){
+      //this is the right arrow key
+      this.x += 5;
+    }//end if
+    
+  }
 }//end Player
