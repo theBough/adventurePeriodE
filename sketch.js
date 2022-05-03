a9d3ffd4d14a24914d762530986f3710d83c61c7
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-03 15:16:48 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,38 @@
-let p;
-let w = []
-let activeX, activeY;
-function setup() {
-  createCanvas(400, 400);
-  p = new Player(50,50,20,20, "#8c1aff");
-  activeX = 0;
-  activeY = 0;
-  fillRooms();
-}
-function draw() {
-  background("#ffff4d");
-  p.display()
-  p.update()
-  for(i=0 ; i<w.length ; i++){
-    //this will loop however many walls
-    //there are in this room
-    w[i].display()
-    
-  }//end loop
-  screenChange();
+function Wall(x,y,w,h,col){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.col = col;
   
-  rooms[activeX][activeY].call()
-  checkForCollision()
-}//end draw
-function screenChange(){
-  /*this function is checking to see if the player has gone off
-  the canvas*/
-  if(p.x < 0){
-    //off the canvas tothe left
-    //so place the player on the right
-    p.x = width - p.w
-  }
-  if(p.x > width){
-    //off the canvas tothe right
-    //so place the player on the left
-    p.x = 0
-  }
-  if(p.y < 0){
-    //off the canvas tothe top
-    //so place the player on the bottom
-    p.y = height - p.h
-  }
-  if(p.y > height){
-    //off the canvas tothe bottom
-    //so place the player on the top
-    p.y = 0
-  }
-}//end screen change
+  this.display = function(){
+    fill(this.col);
+    rect(this.x, this.y , this.w, this.h)
+  }//end display
+}//end Wall
+
+function checkForCollision() {
+  for (var i = 0; i < w.length; i++) {
+
+    //check if we hit the left of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x <= w[i].x + w[i].w && p.x >= w[i].x) {
+      p.x += 5
+    }
+
+    //check if we hit the right of any wall
+    if (p.y <= w[i].y + w[i].h && p.y + p.h >= w[i].y && p.x + p.w >= w[i].x && p.x <= w[i].x + w[i].w) {
+      p.x -= 5
+    }
+
+
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y <= w[i].y + w[i].h && p.y >= w[i].y) {
+      p.y += 5;
+    }
+
+    //check if we hit the top of any wall
+    if (p.x <= w[i].x + w[i].w && p.x + p.w >= w[i].x && p.y + p.h >= w[i].y && p.y <= w[i].y + w[i].h) {
+      p.y -= 5;
+    }
+
+  }//end loop
+}//end colision
