1fec41c28a109808e3014e50b6d4e478bc0c8160
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-03 15:15:11 -0500

    added a second room

---
+++ rooms.js
@@ -0,0 +1,22 @@
+let rooms = []
+function fillRooms(){
+  rooms = [
+  [roomOne, roomTwo]
+]
+}
+/* Don't copy this stuff because you will lose your walls
+*/
+var roomOne = function(){
+  w=[];
+  //Top wall
+  w.push(new Wall(0,0,width,20,"#66ccff"))
+  //wall in the middle
+  w.push(new Wall(0,200,175,20,"#ff66a3"))
+  //green wall going downward
+  w.push(new Wall(300,200,20,200,"#ccff66"))
+ 
+}
+var roomTwo = function(){
+  w=[];
+  w.push(new Wall(380,0,20,height,"#ccff66"))
+}
