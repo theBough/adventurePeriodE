1ac49729a941b9e57e2f7414931839ea0efe3db6
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-11 14:27:40 -0500

    Create gate.js

---
+++ gate.js
@@ -0,0 +1,11 @@
+function Gate(x,y,w,h,r,col){
+  this.x  =x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.col = col;
+  this.r = r
+  this.display = function(){
+    
+  }//end display
+}//end Gate
