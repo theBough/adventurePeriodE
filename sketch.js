11d30a59252d9442ce8bc2aaf89e538dab211030
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-11 14:43:49 -0500

    added a gate

--- sketch.js
+++ sketch.js
@@ -2,10 +2,11 @@ let p;
 let w = []
 let k;
 let activeX, activeY;
+let g;
 function setup() {
   createCanvas(400, 400);
   p = new Player(50,50,20,20, "#8c1aff");
-  
+  g = new Gate(250,50,20,80,roomOne,"white");
   activeX = 0;
   activeY = 0;
   fillRooms();
@@ -18,6 +19,8 @@ function draw() {
   k.display()
   k.keyCollision();
   k.update();
+  g.display();
+  g.gateCollision();
   for(i=0 ; i<w.length ; i++){
     //this will loop however many walls
     //there are in this room
