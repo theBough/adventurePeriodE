f09c8173a227fe47e91f7d8fb0fccb586f93435e
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-05-11 14:39:33 -0500

    Update gate.js

--- gate.js
+++ gate.js
@@ -5,7 +5,57 @@ function Gate(x,y,w,h,r,col){
   this.h = h;
   this.col = col;
   this.r = r
+  this.show = true
+  
   this.display = function(){
-    
+    if(this.r == rooms[activeY][activeX] && this.show ){
+      fill(this.col);
+      rect(this.x, this.y, this.w, this.h);
+    }//end if
   }//end display
+  
+  this.gateCollision = function(){
+     //check if we hit the left of any key
+    if (
+      k.y <= this.y + this.h &&
+      k.y + k.h >= this.y &&
+      k.x <= this.x + this.w &&
+      k.x >= this.x &&
+      this.r === rooms[activeY][activeX]
+    ) {
+     this.show = false
+    }
+
+    //check if we hit the right of any key
+    if (
+      k.y <= this.y + this.h &&
+      k.y + k.h >= this.y &&
+      k.x + k.w >= this.x &&
+      k.x <= this.x + this.w &&
+      this.r === rooms[activeY][activeX]
+    ) {
+      this.show = false
+    }
+
+    if (
+      k.x <= this.x + this.w &&
+      k.x + k.w >= this.x &&
+      k.y <= this.y + this.h &&
+      k.y >= this.y &&
+      this.r === rooms[activeY][activeX]
+    ) {
+      this.show = false
+    }
+
+    //check if we hit the top of any key
+    if (
+      k.x <= this.x + this.w &&
+      k.x + k.w >= this.x &&
+      k.y + k.h >= this.y &&
+      k.y <= this.y + this.h &&
+      this.r === rooms[activeY][activeX]
+    ) {
+      this.show = false
+    }    
+  }//end gate Collision
 }//end Gate
